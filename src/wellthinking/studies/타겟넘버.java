ee450c78702f30befa366230089389920c9ee259
howIsYourDayHuh <woguqdusrk0909@gmail.com>
2023-03-06 20:02:33 +0900

    다시 Commit

---
+++ src/wellthinking/studies/타겟넘버.java
@@ -0,0 +1,55 @@
+package wellthinking.studies;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class 타겟넘버 {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 1, 1, 1, 1};
+        int target = 3;
+
+//        int[] numbers = {4, 1, 2, 1};
+//        int target = 4;
+
+        int answer = 0;
+
+        List<Integer> bigBox = new ArrayList<Integer>();
+
+        bigBox.add(-numbers[0]);
+        bigBox.add(numbers[0]);
+
+        int countOfTargetedNums = 0;
+        int cnt     = numbers.length;
+        for(int index = 1; index <= numbers.length; index++){
+
+            if(index == cnt){
+                break;
+            }
+
+            List<Integer> anotherBox = new ArrayList<Integer>();
+
+            for(int num : bigBox){
+                System.out.println("[index] : " + index + " / [num] : " + num + " / [anotherBox] : " +anotherBox.size());
+
+                anotherBox.add(num + (-numbers[index]));
+                anotherBox.add(num + numbers[index]);
+
+            }
+
+            for(int num : anotherBox){
+                System.out.println("[num]" + num);
+            }
+
+            bigBox = new ArrayList<>(anotherBox);
+
+        }
+
+        answer = Collections.frequency(bigBox, target);
+
+        System.out.println(answer);
+
+
+    }
+}
